0237cb619bd32d6ab70d2add3da99426d15a952c
Brendan <brendanschatz97@gmail.com>
2020-08-08 15:29:51 -0400

    first route

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-const express = require('express');
-const bodyParser = require('body-parser')
+const express = require("express");
 
+const db = require("./data/db.js");
 
 const app = express();
-const PORT = 3000;
 
-app.use(bodyParser.json())
+app.use(express.json());
 
-
-app.listen(PORT, () => {
-    console.log('Listening at ' + PORT );
+/**
+ * METHOD: GET
+ * ROUTE: /todo
+ * PURPOSE: Get all tasks
+ */
+app.get("/todo", async (req, res) => {
+  const todos = await db("todo");
+  res.json({ todos });
 });
+
+/**
+ * METHOD: POST
+ * ROUTE: /todo
+ * PURPOSE: Create new task
+ */
+app.post("/todo", async (req, res) => {
+  const { task } = req.body;
+  const newTodo = await db("todo")
+    .insert(task)
+    .then(item => {
+      return item.rowCount;
+    });
+    
+  if (newTodo === 1) {
+    return res.status(201).json({ message: "Todo created successfully" });
+  }
+});
+
+const PORT = process.env.PORT || 3500;
+
+app.listen(PORT, () => console.log(`app listening on port ${PORT}`));

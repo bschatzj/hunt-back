7bd732aada6200d309e77e4e3491909b067fc2e5
Brendan <brendanschatz97@gmail.com>
2020-08-09 17:13:33 -0400

    more auth stuff

--- Routes/api-router.js
+++ Routes/api-router.js
@@ -1,5 +1,9 @@
 const router = require('express').Router()
+const Login = require('./Auth/Login/Login')
+const Register = require ('./Auth/Register/Register')
 
+router.use('/login' , Login)
+router.use('/register', Register)
 
 
 module.exports = router

6f6bd59d35af394a788084319dff491bfb385159
Brendan <brendanschatz97@gmail.com>
2020-08-10 16:50:52 -0400

    added route

--- Routes/api-router.js
+++ Routes/api-router.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
 const Login = require('./Auth/Login/Login')
 const Register = require ('./Auth/Register/Register')
+const NewGame = require ('./Games/GameRoutes')
 
 router.use('/login' , Login)
 router.use('/register', Register)
+router.use('/game', NewGame)
 
 
 module.exports = router

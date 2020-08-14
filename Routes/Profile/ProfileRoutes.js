c4fa9730e6a6b0c1d85a1795faa58462772ca60e
Brendan <brendanschatz97@gmail.com>
2020-08-14 13:02:09 -0400

    got a few more profile routes

--- Routes/Profile/ProfileRoutes.js
+++ Routes/Profile/ProfileRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express-promise-router')(),
-db = require('../../data/db');
+    db = require('../../data/db');
 const { authenticate } = require('../Auth/Token');
 
 
@@ -12,8 +12,26 @@ router.get('/games/:id', async (req, res) => {
     const id = req.params.id
     console.log(id)
     const games = await db('list')
-    .where('user', id)
+        .where('user', id)
     console.log(games)
 
-    res.status(200).json({"gameList": games})
+    res.status(200).json({ "gameList": games })
+})
+
+router.get('/profile/:id', async (req, res) => {
+    const id = req.params.id
+    const user = await db('users')
+        .where('user_id', id)
+        .first()
+
+    res.status(200).json({profile: user})
+})
+
+router.put('/update/:id', async (req, res) => {
+    const user = await db('users')
+    .where('user_id', id)
+    .first()
+    .update(req.body)
+    .then(res.status(200).json("update complete"))
+    .catch(err => {res.status(500).json(err)})
 })
--- data/migrations/02_users.js
+++ data/migrations/02_users.js
@@ -1,3 +1,5 @@
+const { table } = require("../db");
+
 exports.up = knex =>
   knex.schema.createTable("users", tbl => {
     tbl.increments('user_id');
@@ -7,6 +9,9 @@ exports.up = knex =>
     .unique()
     tbl.text('password')
     .notNullable()
+    tbl.text('display_name')
+    tbl.text('icon')
+    .defaultTo('https://img.icons8.com/cute-clipart/64/000000/bug.png')
   });
 
 exports.down = knex => knex.schema.dropTableIfExists("todo");

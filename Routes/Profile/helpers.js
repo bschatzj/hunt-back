b0c3e707f320855263227480bffbef92e851a9b4
Brendan <brendanschatz97@gmail.com>
2020-08-15 01:05:17 -0400

    updating name as well

--- Routes/Profile/helpers.js
+++ Routes/Profile/helpers.js
@@ -9,6 +9,7 @@ function update(id, update) {
     db('users')
         .where('user_id', update.user_id)
         .update('icon', update.icon)
+        .update('display_name', update.display_name)
         .then(console.log('working!!!'))
         .catch(err => { console.log(err) })
 }

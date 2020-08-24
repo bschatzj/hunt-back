c930911bcb6932ecb38318673939f675ddfa667c
Brendan <brendanschatz97@gmail.com>
2020-08-24 17:06:10 -0400

    missspelling

--- data/migrations/01_games.js
+++ data/migrations/01_games.js
@@ -4,7 +4,7 @@ exports.up = knex =>
     tbl.text('game_title').notNullable().unique();
     tbl.text('password');
     tbl.boolean('private').defaultTo(false);
-    tbl.int('end_date')
+    tbl.integer('end_date')
   });
 
 exports.down = knex => knex.schema.dropTableIfExists("todo");

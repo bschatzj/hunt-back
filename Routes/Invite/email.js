9efe246b993f4408e64d36192463f3dac0a9290d
Brendan Schatz <brendanschatz97@gmail.com>
2022-12-10 22:27:45 -0500

    Updated Packages and Format.

--- Routes/Invite/email.js
+++ Routes/Invite/email.js
@@ -1,32 +1,30 @@
-const router = require('express-promise-router')()
-var nodemailer = require('nodemailer');
-const Password = process.env.EMAILPASSWORD
+const router = require("express-promise-router")();
+var nodemailer = require("nodemailer");
+const Password = process.env.EMAILPASSWORD;
 
 var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'scavengewithfriends@gmail.com',
-    pass: Password
-  }
+	service: "gmail",
+	auth: {
+		user: "scavengewithfriends@gmail.com",
+		pass: Password,
+	},
 });
 
-router.post('/send', (req,res) => {
-    console.log(req.body)
+router.post("/send", (req, res) => {
+	var mailOptions = {
+		from: "scavenge@gmail.com",
+		to: req.body.email,
+		subject: "You have been challenged!",
+		text: req.body.message,
+	};
 
-    var mailOptions = {
-        from: 'scavenge@gmail.com',
-        to: req.body.email,
-        subject: 'You have been challenged!',
-        text: req.body.message,
-      };
-    
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          res.status(400).json(error);
-        } else {
-          res.status(200).json('Email sent: ' + info.response);
-        }
-      });
-})
+	transporter.sendMail(mailOptions, function (error, info) {
+		if (error) {
+			res.status(400).json(error);
+		} else {
+			res.status(200).json("Email sent: " + info.response);
+		}
+	});
+});
 
-module.exports = router
+module.exports = router;
